350e054095c8d02411b09de159107bc54622abdb
wbalaniucGC <113048699+wbalaniucGC@users.noreply.github.com>
2026-03-23 11:48:43 -0400

    Saving changes

--- Assets/Scripts/Mechanics/Collectible/Cherry.cs
+++ Assets/Scripts/Mechanics/Collectible/Cherry.cs
@@ -10,7 +10,7 @@ public class Cherry : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             // Announce that a cherry has been picked up
             onPickedUp.Invoke(cherryValue);
